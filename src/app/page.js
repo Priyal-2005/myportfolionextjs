43405517bf299f38959fd0f239980c279a9cd893
Priyal Sarda <priyal.sarda2005@gmail.com>
2025-06-25 17:24:04 +0530

    page

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,44 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+
+import Image from 'next/image';
+import './globals.css';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <header className="header">
+        <a href="#" className="logo">Portfolio.</a>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <nav className="navbar">
+          <a href="#home" style={{ '--i': 1 }} className="active">Home</a>
+          <a href="#about" style={{ '--i': 2 }}>About</a>
+          <a href="#skills" style={{ '--i': 3 }}>Skills</a>
+          <a href="#projects" style={{ '--i': 4 }}>Portfolio</a>
+          <a href="#contact" style={{ '--i': 5 }}>Contact</a>
+        </nav>
+      </header>
+
+      <section className="home" id="home">
+        <div className="home-content">
+          <h3>Hello, it's Me</h3>
+          <h1>Priyal Sarda</h1>
+          <h3>And I'm a <span className="text"></span></h3>
+          <p>Co-founder of Coding Champs and Founder of PhotoDost.<br />
+            Student entrepreneur passionate about AI, and turning real-world problems into impactful solutions.
+          </p>
+          <div className="profile-container">
+            <img src="/images/profile.jpg" alt="My Photo" className="profile-pic" />
+          </div>
+          <div className="home-sci">
+            <a href="#" style={{ '--i': 7 }}><i className='bx bxl-linkedin'></i></a>
+            <a href="#" style={{ '--i': 8 }}><i className='bx bxl-gmail'></i></a>
+            <a href="#" style={{ '--i': 9 }}><i className='bx bxl-github'></i></a>
+            <a href="#" style={{ '--i': 10 }}><i className='bx bxl-instagram'></i></a>
+          </div>
+          <a href="#" className="btn-box">More About Me</a>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <span className="home-imgHover"></span>
+      </section>
+    </>
   );
 }
